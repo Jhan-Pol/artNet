8c77f6ebcca76d1772d4f8d9f15a573269ac3e2e
CristianAGarces <cristyan@users.noreply.github.com>
2025-03-30 14:20:06 -0500

    cristian

---
+++ artNet/Models/Artista.cs
@@ -0,0 +1,21 @@
+namespace artNet.Models
+{
+    public class Artista
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string photoUrl { get; set; }
+        public string age { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string pinturas { get; set; }
+
+        //public void reaccionar()
+        //{
+
+        //}
+    }
+}
